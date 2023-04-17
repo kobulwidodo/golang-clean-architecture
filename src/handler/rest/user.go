a7b678819a3d1d20a14a8d0a69c3d5250c171a51
kobulwidodo <fadhilhan01@gmail.com>
2023-04-17 14:34:44 +0700

    feat: add swagger documentation

--- src/handler/rest/user.go
+++ src/handler/rest/user.go
@@ -7,6 +7,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary Register User
+// @Description Register New User
+// @Tags Auth
+// @Param user body entity.CreateUserParam true "user info"
+// @Produce json
+// @Success 200 {object} entity.Response{data=entity.User{}}
+// @Failure 400 {object} entity.Response{}
+// @Failure 401 {object} entity.Response{}
+// @Failure 404 {object} entity.Response{}
+// @Failure 500 {object} entity.Response{}
+// @Router /api/v1/auth/register [POST]
 func (r *rest) RegisterUser(ctx *gin.Context) {
 	var userParam entity.CreateUserParam
 	if err := ctx.ShouldBindJSON(&userParam); err != nil {
@@ -23,6 +34,17 @@ func (r *rest) RegisterUser(ctx *gin.Context) {
 	r.httpRespSuccess(ctx, http.StatusCreated, "successfully registered new user", user)
 }
 
+// @Summary Login User
+// @Description Login User
+// @Tags Auth
+// @Param user body entity.LoginUserParam true "user info"
+// @Produce json
+// @Success 200 {object} entity.Response{}
+// @Failure 400 {object} entity.Response{}
+// @Failure 401 {object} entity.Response{}
+// @Failure 404 {object} entity.Response{}
+// @Failure 500 {object} entity.Response{}
+// @Router /api/v1/auth/login [POST]
 func (r *rest) LoginUser(ctx *gin.Context) {
 	var userParam entity.LoginUserParam
 	if err := ctx.ShouldBindJSON(&userParam); err != nil {
